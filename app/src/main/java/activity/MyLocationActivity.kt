7704e15c6ec494bb9387360b3c3d462892008661
munjung <jekang0123@gmail.com>
2019-06-06 23:43:16 +0900

    뭔가 잘못됐다..

--- app/src/main/java/activity/MyLocationActivity.kt
+++ app/src/main/java/activity/MyLocationActivity.kt
@@ -11,6 +11,7 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.RelativeLayout
 import android.widget.Toast
 import com.km.deodeumi.R
